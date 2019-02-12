b36497049cc3dc38cd0cc3cde0e53469c68fdff8
cyliang1113 <cyliang1113@126.com>
2019-02-12 09:24:05 +0800

    111

--- src/test/java/cn/leo/lab/activitidemo/QingJiaProcessTest.java
+++ src/test/java/cn/leo/lab/activitidemo/QingJiaProcessTest.java
@@ -2,6 +2,7 @@ package cn.leo.lab.activitidemo;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.runtime.ProcessInstance;
@@ -13,8 +14,8 @@ import org.junit.Test;
 
 import java.util.List;
 
-public class ProcessTest {
-    private static Log log = LogFactory.getLog(ProcessTest.class);
+public class QingJiaProcessTest {
+    private static Log log = LogFactory.getLog(QingJiaProcessTest.class);
 
     private static ProcessEngine processEngine = null;
 
@@ -26,12 +27,11 @@ public class ProcessTest {
     /**
      * 部署流程
      */
-    //@Test
+    @Test
     public void deploy() {
-
         DeploymentBuilder deployment = processEngine.getRepositoryService().createDeployment();
-        deployment.addClasspathResource("qingjialiucheng.bpmn");
-        deployment.addClasspathResource("qingjialiucheng.png");
+        deployment.addClasspathResource("process/qingjia/qingjialiucheng.bpmn");
+        deployment.addClasspathResource("process/qingjia/qingjialiucheng.png");
         deployment.name("请假流程部署");
         Deployment deploy = deployment.deploy();
         log.info(deploy.getId());
@@ -39,12 +39,24 @@ public class ProcessTest {
     }
 
     /**
+     * 删除deploy
+     */
+    @Test
+    public void delDeploy(){
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        String deployId = "1";
+        repositoryService.deleteDeployment(deployId);
+    }
+
+    /**
      * 开始请假流程
      */
-    //@Test
+    @Test
     public void qingjiaStart(){
-        String liuchengId = "qingjialiucheng:1:4";
-        ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceById(liuchengId);
+//        String liuchengId = "qingjialiucheng:1:4";
+//        ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceById(liuchengId);
+        String key = "qingjialiucheng";
+        ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey(key);
         log.info(processInstance.getId());
         log.info(processInstance.getName());
     }
@@ -52,23 +64,24 @@ public class ProcessTest {
     /**
      * 查询流程
      */
-    //@Test
+    @Test
     public void queryProcess(){
         TaskQuery taskQuery = processEngine.getTaskService().createTaskQuery();
-        taskQuery.taskAssignee("lisi");
+        //taskQuery.taskAssignee("lisi");
         List<Task> list = taskQuery.list();
         for(Task t : list){
             log.info(t.getName());
             log.info(t.getId());
+            log.info(t.getTaskLocalVariables());
         }
     }
 
     /**
      * 请假
      */
-    //@Test
+    @Test
     public void qingjia(){
-        String taskId = "2504";
+        String taskId = "5004";
         processEngine.getTaskService().complete(taskId);
 
     }
@@ -78,7 +91,7 @@ public class ProcessTest {
      */
     @Test
     public void qingjiashenpi(){
-        String taskId = "5002";
+        String taskId = "7502";
         processEngine.getTaskService().complete(taskId);
 
     }
